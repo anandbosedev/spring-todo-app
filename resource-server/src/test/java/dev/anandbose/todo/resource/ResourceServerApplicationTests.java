d3347bbb15208ed6ae4e5c74c6c91116d42d64cb
Anand Bose <anandbose16@gmail.com>
2025-06-06 14:53:34 +0530

    Create projects authorization-server and resource-server

---
+++ resource-server/src/test/java/dev/anandbose/todo/resource/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anandbose.todo.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
